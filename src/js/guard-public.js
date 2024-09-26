e03dc0da1c6b8423f79567a1bb56887bdd345795
bakazero <bobzzee@gmail.com>
2024-09-26 14:42:16 +0700

    coba slug uri

--- src/js/guard-public.js
+++ src/js/guard-public.js
@@ -1,5 +1,5 @@
 import { getAuth } from "./libraries/cookies.js";
-import { baseUrl } from "./customs/settings.js";
+import { slugUri } from "./customs/settings.js";
 
 const auth = await getAuth();
-if (auth) window.location.replace(`${baseUrl}beranda`);
+if (auth) window.location.replace(`${slugUri}beranda`);
